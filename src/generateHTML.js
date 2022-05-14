3276b74fdd55fe17eae287ef36711ebcfcb61cec
Mitchell <mitchellm27@gmail.com>
2022-05-14 09:16:40 -0700

    added intern html layout.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -17,3 +17,22 @@ const generateManager = function (manager) {
     </div>
     `;
 }
+
+const generateIntern = function (intern) {
+    return `
+    <div class="col-5 mt-5">
+        <div class="card h-100">
+            <div class="card-header">
+                <h2>${intern.name}</h2>
+                <h3>Intern</h3><i class="material-icons">assignment_ind</i>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+    </div>
+</div>
+    `
+};
